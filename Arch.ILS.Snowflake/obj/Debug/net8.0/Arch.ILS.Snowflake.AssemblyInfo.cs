1435025d1817ab535642e1cacd2b8d8e595cb6c3
Antonello Tosco <atosco@archre.com>
2024-09-10 08:48:37 -0300

    Arch.ILS.EconomicModel - Correct YeltPartitioner

--- Arch.ILS.Snowflake/obj/Debug/net8.0/Arch.ILS.Snowflake.AssemblyInfo.cs
+++ Arch.ILS.Snowflake/obj/Debug/net8.0/Arch.ILS.Snowflake.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arch.ILS.Snowflake")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ea2290d301620ab0b412289acca22a754aee0bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e45048ed48272234ac927f99b98400359b7ad696")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arch.ILS.Snowflake")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arch.ILS.Snowflake")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
